b95672fc2b4e9034750ad698820dbc460bbec3fc
Catie Xun <catiexun@gmail.com>
2025-10-27 17:37:56 -0700

    updated source, written, writtenText functions

--- ts/tweet.ts
+++ ts/tweet.ts
@@ -10,13 +10,31 @@ class Tweet {
 	//returns either 'live_event', 'achievement', 'completed_event', or 'miscellaneous'
     get source():string {
         //TODO: identify whether the source is a live event, an achievement, a completed event, or miscellaneous.
-        return "unknown";
+        if (this.text.startsWith('Just completed') || this.text.startsWith('Just posted')) {
+            return "completed_event";
+        }
+
+        else if (this.text.startsWith("Watch my")) {
+            return "live_event";
+        }
+
+        else if (this.text.startsWith("Achieved a new personal record with #Runkeeper")) {
+            return "achievement";
+        }
+
+        else {
+            return "miscellaneous";
+        }
     }
 
     //returns a boolean, whether the text includes any content written by the person tweeting.
     get written():boolean {
-        //TODO: identify whether the tweet is written
-        return false;
+        if (this.text.includes("Check it out!") || this.text.includes("@Runkeeper Live") || this.text.includes("TomTom MySports Watch")) {
+            return false;
+        }
+        else {
+            return true;
+        }
     }
 
     get writtenText():string {
@@ -24,7 +42,14 @@ class Tweet {
             return "";
         }
         //TODO: parse the written text from the tweet
-        return "";
+        const startMarker = " - ";
+        const endMarker = "https://t.co/";
+
+        const startIndex = this.text.indexOf(startMarker) + startMarker.length;
+        const endIndex = this.text.indexOf(endMarker, startIndex);
+        const writtenText = this.text.substring(startIndex, endIndex);
+
+        return writtenText;
     }
 
     get activityType():string {
